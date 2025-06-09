f810531ec48d10d7c260faf206eada24b5671a49
ashuguptajsx <ashugupta1403@gmail.com>
2025-06-09 13:59:08 +0530

    commit

--- backend/jango/views.py
+++ backend/jango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import ChaiVarity
 
 # Create your views here.
 
 def all_jango(request):
-    return render(request, 'jango/all_jango.html')
+    chais = ChaiVarity.objects.all()
+    return render(request, 'jango/all_jango.html',{'chais':chais})
